6ebb7187a8625e6220c568bf1e12da258cb2a681
unknown <micheal1242002@gmail.com>
2025-08-10 10:13:38 +0300

    change main.js

--- script.js
+++ script.js
@@ -84,7 +84,6 @@ function toggleContent() {
 // Scroll to Top Button
   const scrollBtn = document.getElementById('scrollToTopBtn');
 
-  
   window.addEventListener('scroll', () => {
     if (window.scrollY > 300) {
       scrollBtn.classList.remove('hidden');
